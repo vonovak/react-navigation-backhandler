bd8bfd423a2879c0d40005c7a446913f6576ce4c
Radu Batori <radubatori@luminos.software>
2018-06-14 11:15:53 +0300

    Add TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-navigation-backhandler' {
+  import { Component } from 'react';
+
+  export interface AndroidBackHandlerProperties {
+    onBackPress: () => boolean;
+  }
+
+  export class AndroidBackHandler extends Component<AndroidBackHandlerProperties> {}
+}
