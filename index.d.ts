2f4915e0b6bbc3f5fffcb6215747dbac66020add
Vojtech Novak <vonovak@gmail.com>
2020-03-30 10:39:00 +0200

    fix: add hook TS typing

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
-declare module 'react-navigation-backhandler' {
-  import { Component } from 'react';
+declare module "react-navigation-backhandler" {
+  import { Component } from "react";
 
   export interface AndroidBackHandlerProperties {
     onBackPress: () => boolean;
   }
 
-  export class AndroidBackHandler extends Component<AndroidBackHandlerProperties> {}
+  export class AndroidBackHandler extends Component<
+    AndroidBackHandlerProperties
+  > {}
+
+  export function useAndroidBackHandler(onBackPress: () => boolean);
 }
